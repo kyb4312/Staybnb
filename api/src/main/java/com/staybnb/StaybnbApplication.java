24ff02ee4b279318e53225693cbc227b61c7c96e
Yebin Kim <yebinkim@Yebinui-MacBookAir.local>
2025-05-12 18:34:58 +0900

    init: 멀티 모듈 프로젝트 초기 설정

---
+++ api/src/main/java/com/staybnb/StaybnbApplication.java
@@ -0,0 +1,13 @@
+package com.staybnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaybnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaybnbApplication.class, args);
+    }
+
+}

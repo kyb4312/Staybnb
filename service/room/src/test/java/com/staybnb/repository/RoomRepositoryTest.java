6c99a05454a495772de45d4c3858eff043ecee81
Yebin Kim <kyb4312@gmail.com>
2025-05-24 23:14:19 +0900

    test: RoomRepositoryTest 추가

---
+++ service/room/src/test/java/com/staybnb/repository/RoomRepositoryTest.java
@@ -0,0 +1,98 @@
+package com.staybnb.repository;
+
+import com.staybnb.domain.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RoomRepositoryTest {
+
+    private final RoomRepository roomRepository = new MemoryRoomRepository();
+
+    @Test
+    void crudTest() {
+        // save
+        Address address = Address.builder()
+                .country("United States")
+                .province("Kentucky")
+                .city("Louisville")
+                .street("610 W Magnolia Ave")
+                .build();
+
+        List<Amenity> amenities = new ArrayList<>();
+        amenities.add(Amenity.WIFI);
+        amenities.add(Amenity.AIR_CONDITIONER);
+        amenities.add(Amenity.TV);
+        amenities.add(Amenity.KITCHEN);
+
+        Room room = Room.builder()
+                .hostId(1L)
+                .placeType(PlaceType.HOUSE)
+                .roomType(RoomType.ENTIRE_PLACE)
+                .address(address)
+                .maxNumberOfGuests(2)
+                .bedrooms(1)
+                .beds(1)
+                .amenities(amenities)
+                .title("Modern house in Kentucky")
+                .description("Modern house in Kentucky")
+                .pricePerNight(700_000)
+                .currency(Currency.KRW)
+                .build();
+
+        Room roomCreated = roomRepository.save(room);
+        long roomId = roomCreated.getId();
+
+        // findById
+        Room roomFound = roomRepository.findById(roomId).orElse(null);
+        assertThat(roomFound).isEqualTo(roomCreated);
+
+        // findAll
+        // 검색 조건에 맞는 숙소 찾음
+        RoomSearchCondition searchCondition = RoomSearchCondition.builder()
+                .numberOfGuests(2)
+                .location("Kentucky")
+                .build();
+        List<Room> rooms = roomRepository.findAll(searchCondition);
+        assertThat(rooms).hasSize(1);
+
+        // 검색 조건에 맞는 숙소 없음
+        searchCondition = RoomSearchCondition.builder()
+                .numberOfGuests(2)
+                .priceTo(500_000)
+                .build();
+        rooms = roomRepository.findAll(searchCondition);
+        assertThat(rooms).hasSize(0);
+
+        // update
+        RoomUpdateInfo updateInfo = RoomUpdateInfo.builder()
+                .maxNumberOfGuests(4)
+                .bedrooms(2)
+                .beds(2)
+                .pricePerNight(800_000)
+                .build();
+
+        Room roomUpdated = roomRepository.update(roomId, updateInfo);
+
+        assertThat(roomUpdated.getId()).isEqualTo(roomId);
+        assertThat(roomUpdated.getMaxNumberOfGuests()).isEqualTo(updateInfo.getMaxNumberOfGuests());
+        assertThat(roomUpdated.getBedrooms()).isEqualTo(updateInfo.getBedrooms());
+        assertThat(roomUpdated.getBeds()).isEqualTo(updateInfo.getBeds());
+        assertThat(roomUpdated.getPricePerNight()).isEqualTo(updateInfo.getPricePerNight());
+
+        // delete
+        Room roomDeleted = roomRepository.findById(roomId).orElse(null);
+        assertThat(roomDeleted).isNotNull();
+        assertThat(roomDeleted.isDeleted()).isFalse();
+
+        roomRepository.delete(roomId);
+
+        roomDeleted = roomRepository.findById(roomId).orElse(null);
+        assertThat(roomDeleted).isNotNull();
+        assertThat(roomDeleted.isDeleted()).isTrue();
+        assertThat(roomDeleted.getDeletedAt()).isNotNull();
+    }
+}

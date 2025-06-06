0ee3f18c9bbf8a2e4a484b76b3026f9e8593ebb3
Yebin Kim <kyb4312@gmail.com>
2025-06-06 21:54:01 +0900

    refactor: DB 데이터 24시간마다 캐싱하도록 수정 (Amenity, Currency, PlaceType)

--- service/room/src/main/java/com/staybnb/rooms/service/AmenityService.java
+++ service/room/src/main/java/com/staybnb/rooms/service/AmenityService.java
@@ -1,27 +1,26 @@
 package com.staybnb.rooms.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.staybnb.rooms.domain.Amenity;
 import com.staybnb.rooms.repository.AmenityRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AmenityService {
 
     private final AmenityRepository amenityRepository;
-    private final Map<String, Amenity> amenityMap = new HashMap<>();
+    private final Cache<String, Amenity> amenityCache = Caffeine.newBuilder().build();
 
-    @PostConstruct
-    public void loadAmenityMap() {
-        amenityRepository.findAll().forEach(amenity -> amenityMap.put(amenity.getName(), amenity));
+    @Scheduled(initialDelay = 0, fixedDelay = 24 * 60 * 60 * 1000)
+    private void refreshAmenityCache() {
+        amenityRepository.findAll().forEach(amenity -> amenityCache.put(amenity.getName(), amenity));
     }
 
     public Amenity getByName(String name) {
-        return amenityMap.get(name);
+        return amenityCache.getIfPresent(name);
     }
 }

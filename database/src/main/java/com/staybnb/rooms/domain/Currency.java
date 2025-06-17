2254364d188146248088ba3d021708f800eb0acf
Yebin Kim <kyb4312@gmail.com>
2025-06-17 18:26:25 +0900

    fix: Currency Setter 삭제된 건 수정
    생성자 만들 때 삭제됐던 Setter 복구

--- database/src/main/java/com/staybnb/rooms/domain/Currency.java
+++ database/src/main/java/com/staybnb/rooms/domain/Currency.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Currency {
 

7fdefc54fa25f9b834633e59881789ef1c0a6dd7
Yebin Kim <kyb4312@gmail.com>
2025-06-22 21:25:56 +0900

    feat: ExceptionHandler 항목 추가

--- api/src/main/java/com/staybnb/handler/ExceptionControllerAdvice.java
+++ api/src/main/java/com/staybnb/handler/ExceptionControllerAdvice.java
@@ -1,5 +1,8 @@
-package com.staybnb.rooms.handler;
+package com.staybnb.handler;
 
+import com.staybnb.bookings.exception.BookingPriceChangedException;
+import com.staybnb.bookings.exception.ExceededNumberOfGuestException;
+import com.staybnb.bookings.exception.UnavailableDateException;
 import com.staybnb.rooms.dto.response.ExceptionResponse;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -36,6 +39,24 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleNoSuchElementException(NoSuchElementException e) {
+        return new ExceptionResponse("ERROR", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ExceptionResponse handleBookingPriceChangedException(BookingPriceChangedException e) {
+        return new ExceptionResponse("ERROR", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleExceededNumberOfGuestsException(ExceededNumberOfGuestException e) {
+        return new ExceptionResponse("ERROR", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleUnavailableDateException(UnavailableDateException e) {
         return new ExceptionResponse("ERROR", e.getMessage());
     }
 }

ed17d88f40b77989ae8bb5df79dbfa19abfd5940
Yebin Kim <kyb4312@gmail.com>
2025-06-23 16:12:19 +0900

    chore: SQL scripts 수정
    예약 상태 ENDED로 바뀌는 것 확인 용 데이터 추가

--- database/src/main/resources/data.sql
+++ database/src/main/resources/data.sql
@@ -49,7 +49,11 @@ insert into booking (room_id, guest_id, check_in, check_out, number_of_guests, b
 values (2, 2, now() + interval 8 day, now() + interval 10 day, 2, 550000, "KRW", 'REJECTED');
 
 insert into booking (room_id, guest_id, check_in, check_out, number_of_guests, booking_price, currency, status)
-values (2, 2, now() + interval 10 day, now() + interval 12 day, 2, 550000, "KRW", 'ENDED');
+values (2, 2, now() - interval 10 day, now() - interval 12 day, 2, 550000, "KRW", 'ENDED');
+
+-- booking (to be updated)
+insert into booking (room_id, guest_id, check_in, check_out, number_of_guests, booking_price, currency, status)
+values (2, 2, now() - interval 2 day, now() - interval 1 day, 2, 550000, "KRW", 'RESERVED');
 
 -- availability
 insert into availability (room_id, start_date, end_date, is_available)

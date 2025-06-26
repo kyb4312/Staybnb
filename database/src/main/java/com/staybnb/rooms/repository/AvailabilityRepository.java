05142190d6369a2886425346f956c138af1a4db0
Yebin Kim <kyb4312@gmail.com>
2025-06-26 22:20:22 +0900

    fix: repository 수정 (Booking, Pricing, Availability)
    daterange 타입 연산 사용을 위해 querydsl로 작성했던 custom repository 삭제 후 native query로 변경함

--- database/src/main/java/com/staybnb/rooms/repository/AvailabilityRepository.java
+++ database/src/main/java/com/staybnb/rooms/repository/AvailabilityRepository.java
@@ -1,8 +1,66 @@
 package com.staybnb.rooms.repository;
 
 import com.staybnb.rooms.domain.Availability;
-import com.staybnb.rooms.repository.custom.AvailabilityRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.repository.query.Param;
 
-public interface AvailabilityRepository extends JpaRepository<Availability, Long>, AvailabilityRepositoryCustom {
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+
+    /**
+     * room_id가 같고,
+     * date_range가 [startDateInclusive, endDateExclusive) 구간과 겹치는 availability 조회
+     */
+    @NativeQuery(value = """
+            SELECT * FROM availability
+            WHERE room_id = :roomId
+                AND date_range && daterange(:startDate, :endDate, '[)')
+            """)
+    List<Availability> findAvailabilitiesByDate(
+            @Param("roomId") Long roomId,
+            @Param("startDate") LocalDate startDateInclusive,
+            @Param("endDate") LocalDate endDateExclusive
+    );
+
+    /**
+     * is_available이 true 이고,
+     * room_id가 같고,
+     * date_range가 [startDateInclusive, endDateExclusive) 구간과 겹치는 availability 조회
+     */
+    @NativeQuery(value = """
+            SELECT * FROM availability
+            WHERE is_available = true
+                AND room_id = :roomId
+                AND date_range && daterange(:startDate, :endDate, '[)')
+            ORDER BY lower(date_range)
+            """)
+    List<Availability> findTrueAvailabilitiesByDate(
+            @Param("roomId") Long roomId,
+            @Param("startDate") LocalDate startDateInclusive,
+            @Param("endDate") LocalDate endDateExclusive
+    );
+
+    /**
+     * is_available이 true 이고,
+     * room_id가 같고,
+     * date_range가 [startDateInclusive, endDateExclusive) 구간과 겹치는 availability 를
+     * Row-level 락을 가지고 조회
+     */
+    @NativeQuery(value = """
+            SELECT * FROM availability
+            WHERE is_available = true
+                AND room_id = :roomId
+                AND date_range && daterange(:startDate, :endDate, '[)')
+            ORDER BY lower(date_range)
+            FOR UPDATE
+            """)
+    List<Availability> findTrueAvailabilitiesByDateForUpdate(
+            @Param("roomId") Long roomId,
+            @Param("startDate") LocalDate startDateInclusive,
+            @Param("endDate") LocalDate endDateExclusive
+    );
+
 }
--- database/src/main/java/com/staybnb/rooms/repository/PricingRepository.java
+++ database/src/main/java/com/staybnb/rooms/repository/PricingRepository.java
@@ -1,8 +1,23 @@
 package com.staybnb.rooms.repository;
 
 import com.staybnb.rooms.domain.Pricing;
-import com.staybnb.rooms.repository.custom.PricingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.repository.query.Param;
 
-public interface PricingRepository extends JpaRepository<Pricing, Long>, PricingRepositoryCustom {
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PricingRepository extends JpaRepository<Pricing, Long> {
+
+    @NativeQuery(value = """
+            SELECT * FROM pricing
+            WHERE room_id = :roomId
+                AND date_range && daterange(:startDate, :endDate, '[)')
+            """)
+    List<Pricing> findPricingsByDate(
+            @Param("roomId") Long roomId,
+            @Param("startDate") LocalDate startDateInclusive,
+            @Param("endDate") LocalDate endDateExclusive
+    );
 }

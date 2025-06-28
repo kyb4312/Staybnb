5d5008a3faf76eb7ea042a6830bb271391d8823e
Yebin Kim <kyb4312@gmail.com>
2025-06-28 16:12:07 +0900

    chore: 사용하지 않는 exception 클래스 삭제

--- api/src/main/java/com/staybnb/common/exception/custom/InvalidRoomIdException.java
+++
@@ -1,7 +0,0 @@
-package com.staybnb.common.exception.custom;
-
-public class InvalidRoomIdException extends IllegalArgumentException {
-    public InvalidRoomIdException(Long roomId) {
-        super("존재하지 않는 roomId 입니다. roomId : " + roomId);
-    }
-}
